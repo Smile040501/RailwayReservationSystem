7a5a97caa12b8381a085b4ba6822bcd723e3af45
Mayank Singla <singlamayank001@gmail.com>
2022-05-10 00:06:07 +0530

    Fixed bug of revoking permission for get_passenger_details function from all

--- src/roles/users.sql
+++ src/roles/users.sql
@@ -5,16 +5,10 @@ REVOKE ALL ON PROCEDURE book_tickets,
     allocate_seat
 FROM PUBLIC;
 
-REVOKE ALL ON FUNCTION get_passenger_details
-FROM PUBLIC;
-
 GRANT ALL ON TABLE users TO users;
 GRANT ALL ON TABLE passenger TO users;
 GRANT SELECT ON TABLE ticket TO users;
 
 GRANT EXECUTE ON PROCEDURE book_tickets,
     cancel_booking
-TO users;
-
-GRANT EXECUTE ON FUNCTION get_passenger_details
 TO users;
